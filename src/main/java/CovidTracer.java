a8d89b86daa120cf72c08b97d29a7588956f904d
Kamil CAGLAR <kamilcaglar.contact@gmail.com>
2020-05-14 13:22:53 +0200

    move functions from main to new class

---
+++ src/main/java/CovidTracer.java
@@ -0,0 +1,181 @@
+import dto.Chain;
+import dto.Person;
+import dto.Tree;
+import utils.FileReader;
+import utils.Parser;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
+
+public class CovidTracer {
+    List<URL> url_files;
+
+    public CovidTracer(List<URL> urls) {
+        url_files = urls;
+    }
+
+    /**
+     * Main function of program
+     **/
+    public void launchAnalysis() {
+        List<Person> people = new ArrayList<>();
+
+        try {
+            analyseFiles(people);
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+
+        algorithm(people);
+    }
+
+    /**
+     * Main function of project.
+     * Create output result in new file.
+     *
+     * @throws IOException
+     */
+    public void analyseFiles(List<Person> people) throws IOException {
+        Parser parser = new Parser();
+        List<FileReader> fileReaders = new ArrayList<>();
+
+        // Open each file
+        for (URL country_url : url_files) {
+            FileReader fileReader = new FileReader();
+            fileReaders.add(fileReader);
+            fileReader.openFile(country_url);
+        }
+
+        // Read each file and construct trees
+        List<Person> initialList = new ArrayList<>();
+        for (FileReader fileReader : fileReaders) {
+            String read_string = fileReader.readLine();
+            if (!read_string.equals("")) {
+                Person read_person = parser.parseLine(fileReader.getCountry(), read_string);
+                // Add person to tree
+                initialList.add(read_person);
+            } else {
+                fileReader.closeFile(); // TODO mark file as ended
+            }
+        }
+
+        boolean end;
+        do {
+            // Find the file with the oldest person
+            Person next_person = initialList.get(0);
+            for (Person person : initialList) {
+                if (person.getDiagnosed_ts() < next_person.getDiagnosed_ts()) {
+                    next_person = person;
+                }
+            }
+
+            Person cloned_person = next_person.clone();
+            people.add(cloned_person);
+
+            int index_next_person = initialList.indexOf(next_person);
+
+            String read_string = fileReaders.get(index_next_person).readLine();
+
+            if (!read_string.equals("")) {
+                initialList.set(index_next_person, parser.parseLine(fileReaders.get(index_next_person).getCountry(), read_string));
+            } else {
+                fileReaders.remove(fileReaders.get(initialList.indexOf(next_person)));
+                initialList.remove(next_person);
+            }
+
+            end = fileReaders.isEmpty();
+        }
+        while (!end);
+    }
+
+    public void algorithm(List<Person> people) {
+
+//        // UNIT TEST 2
+//        List<Person> people = new ArrayList<>();
+//        people.add(new Person(1, 1584540000, -1));
+//        people.add(new Person(3, 1584558000, 1));
+//        people.add(new Person(2, 1584712800, -1));
+//        people.add(new Person(4, 1585324800, -1));
+//        people.add(new Person(7, 1585357200, 1));
+
+//        // UNIT TEST 3
+//        List<Person> people = new ArrayList<>();
+//        people.add(new Person(1, 1584540000, -1));
+//        people.add(new Person(2, 1584540000, -1));
+
+//        // UNIT TEST 4
+//        List<Person> people = new ArrayList<>();
+//        people.add(new Person(1, 1584489600, -1));
+//        people.add(new Person(2, 1584576000, 1));
+//        people.add(new Person(3, 1584662400, -1));
+//        people.add(new Person(4, 1584748800, -1));
+//        people.add(new Person(5, 1585267200, 3));
+//        people.add(new Person(6, 1585440000, 4));
+//        people.add(new Person(7, 1586390400, 5));
+//        people.add(new Person(8, 1586908800, 6));
+
+//        // UNIT TEST 9
+//        List<Person> people = new ArrayList<>();
+//        people.add(new Person(1, 1583071200, -1));
+//        people.add(new Person(2, 1583676000, 1));
+//        people.add(new Person(3, 1583679600, 2));
+//        people.add(new Person(4, 1584280800, 3));
+//        people.add(new Person(5, 1584284400, 4));
+//        people.add(new Person(6, 1585501200, 4));
+
+        List<Tree> trees = new ArrayList<>();
+        List<Chain> global_chains = new ArrayList<>();
+
+        for (Person p : people) {
+            // Clear all the chains
+            global_chains.clear();
+
+            // Recalculate every score of every chain
+            ListIterator<Tree> iterator = trees.listIterator();
+            while (iterator.hasNext()) {
+                Tree t = iterator.next();
+                t.updateChains(p.getDiagnosed_ts());
+                if (t.getRoot() == null)
+                    iterator.remove();
+            }
+
+            // If the person is contaminated by someone unknown
+            if (p.getContaminated_by_id() == -1) {
+                trees.add(new Tree(p));
+            } else {
+                // If we found the person who contaminated the new person
+                boolean added = false;
+                for (Tree t : trees) {
+                    if (t.addPerson(p, null)) {
+                        added = true;
+                        break;
+                    }
+                }
+                // If we didn't find him, we create a new tree where the person will be the root
+                if (!added) {
+                    trees.add(new Tree(p));
+                }
+            }
+
+
+            for (Tree t : trees) {
+                global_chains.addAll(t.getChains());
+            }
+
+
+            global_chains.sort(Collections.reverseOrder());
+            for (Chain c : global_chains) System.out.print(c.toString());
+            System.out.print('\n');
+        }
+
+        System.out.println("\nFin du programme");
+    }
+
+
+}
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import dto.Chain;
 import dto.Person;
 import dto.Tree;
 
+import java.io.File;
 import java.util.*;
 
 import utils.FileReader;
@@ -15,148 +16,19 @@ import java.util.List;
 public class Main {
     private static final ArrayList<URL> urls = new ArrayList<>();
 
-    private static final URL france = Main.class.getResource("/data/20/France.csv");
-    private static final URL italy = Main.class.getResource("/data/20/Italy.csv");
-    private static final URL spain = Main.class.getResource("/data/20/Spain.csv");
+    private static final URL france = Main.class.getResource("/data/5000/France.csv");
+    private static final URL italy = Main.class.getResource("/data/5000/Italy.csv");
+    private static final URL spain = Main.class.getResource("/data/5000/Spain.csv");
 
     public static void main(String[] args) {
-
-        List<Person> people = new ArrayList<>();
-
-        try {
-            analyseFiles(people);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        algorithm(people);
-    }
-
-    /**
-     * Main function of project.
-     * Create output result in new file.
-     *
-     * @throws IOException
-     */
-    public static void analyseFiles(List<Person> people) throws IOException {
-        Parser parser = new Parser();
-
+        List<URL> files = new ArrayList<>();
         urls.add(france);
         urls.add(italy);
         urls.add(spain);
+        CovidTracer covidTracer = new CovidTracer(urls);
 
-        List<FileReader> fileReaders = new ArrayList<>();
-
-        // Open each file
-        for (URL country_url : urls) {
-            FileReader fileReader = new FileReader();
-            fileReaders.add(fileReader);
-            fileReader.openFile(country_url);
-        }
-
-        // Read each file and construct trees
-        // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        // ! At the moment the lecture is File by File !
-        // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        for (FileReader fileReader : fileReaders) {
-
-            boolean end = false;
-            do {
-                String read_string = fileReader.readLine();
-                if (!read_string.equals("")) {
-                    Person read_person = parser.parseLine(fileReader.getCountry(), read_string);
-
-                    // Add person to tree
-                    people.add(read_person);
-                } else {
-                    // end of file
-                    end = true;
-                    fileReader.closeFile();
-                }
-            } while (!end);
-        }
+        covidTracer.launchAnalysis();
     }
 
-    public static void algorithm(List<Person> people) {
 
-//        // UNIT TEST 2
-//        List<Person> people = new ArrayList<>();
-//        people.add(new Person(1, 1584540000, -1));
-//        people.add(new Person(3, 1584558000, 1));
-//        people.add(new Person(2, 1584712800, -1));
-//        people.add(new Person(4, 1585324800, -1));
-//        people.add(new Person(7, 1585357200, 1));
-
-//        // UNIT TEST 3
-//        List<Person> people = new ArrayList<>();
-//        people.add(new Person(1, 1584540000, -1));
-//        people.add(new Person(2, 1584540000, -1));
-
-//        // UNIT TEST 4
-//        List<Person> people = new ArrayList<>();
-//        people.add(new Person(1, 1584489600, -1));
-//        people.add(new Person(2, 1584576000, 1));
-//        people.add(new Person(3, 1584662400, -1));
-//        people.add(new Person(4, 1584748800, -1));
-//        people.add(new Person(5, 1585267200, 3));
-//        people.add(new Person(6, 1585440000, 4));
-//        people.add(new Person(7, 1586390400, 5));
-//        people.add(new Person(8, 1586908800, 6));
-
-//        // UNIT TEST 9
-//        List<Person> people = new ArrayList<>();
-//        people.add(new Person(1, 1583071200, -1));
-//        people.add(new Person(2, 1583676000, 1));
-//        people.add(new Person(3, 1583679600, 2));
-//        people.add(new Person(4, 1584280800, 3));
-//        people.add(new Person(5, 1584284400, 4));
-//        people.add(new Person(6, 1585501200, 4));
-
-        List<Tree> trees = new ArrayList<>();
-        List<Chain> global_chains = new ArrayList<>();
-
-        for (Person p : people) {
-            // Clear all the chains
-            global_chains.clear();
-
-            // Recalculate every score of every chain
-            ListIterator<Tree> iterator = trees.listIterator();
-            while (iterator.hasNext()) {
-                Tree t = iterator.next();
-                t.updateChains(p.getDiagnosed_ts());
-                if (t.getRoot() == null)
-                    iterator.remove();
-            }
-
-            // If the person is contaminated by someone unknown
-            if (p.getContaminated_by_id() == -1) {
-                trees.add(new Tree(p));
-            } else {
-                // If we found the person who contaminated the new person
-                boolean added = false;
-                for (Tree t : trees) {
-                    if (t.addPerson(p, null)) {
-                        added = true;
-                        break;
-                    }
-                }
-                // If we didn't find him, we create a new tree where the person will be the root
-                if (!added) {
-                    trees.add(new Tree(p));
-                }
-            }
-
-
-            for (Tree t : trees) {
-                global_chains.addAll(t.getChains());
-            }
-
-
-            Collections.sort(global_chains, Collections.reverseOrder());
-            for (Chain c : global_chains) System.out.print(c.toString());
-            System.out.print('\n');
-        }
-
-        System.out.println("\nFin du programme");
-    }
 }
